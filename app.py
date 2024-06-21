24c67f3552ea4b2ec6ba78975657aa66c9985c5d
Alifah25 <alifaqil2506@gmail.com>
2024-06-22 01:24:23 +0700

    First Commit

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from werkzeug.utils import secure_filename
 
 from pymongo import MongoClient
 
-MONGODB_CONNECTION_STRING = "mongodb+srv://Alifah:sparta@cluster0.lh6vlnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(MONGODB_CONNECTION_STRING)
-db = client.Sekolah
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
